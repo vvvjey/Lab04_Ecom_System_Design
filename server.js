a18cc46eb5a9afffc639610c37fc696082060c98
vvvjey <hoangtu4520031234@gmail.com>
2024-05-23 11:43:43 +0700

    make it better

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ app.set('views', path.join(__dirname,'src', 'resources','views'));
 app.use(express.static(__dirname + '/src/resources'));
 
 
-const port = 3000 
+const port = process.env.PORT || 80 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
   app.use(bodyParser.json());
   initWebRoutes(app);
-console.log('hello',process.env.PORT)
+console.log('hello',process.env.PORT || 80)
 
-app.listen(port || process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port || process.env.PORT}`)
 })
